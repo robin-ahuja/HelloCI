bffd78c2f9817441b1e699e0a488c5d1ca7d32ed
Robin Ahuja <robin.ahuja@basware.com>
2018-09-13 18:11:15 +0530

    test class

---
+++ src/test/HelloCI.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HelloCI.Tests
+{
+    [TestFixture]
+    class Class1
+    {
+        [Test]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Test()
+        {
+            int i = 10, j = 0, x;
+            x = i / j;
+        }
+    }
+}
