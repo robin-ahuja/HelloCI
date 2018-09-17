cb66036d1b57019f3c726d379dfc102bf69605d4
Robin Ahuja <robin.ahuja@basware.com>
2018-09-17 14:24:09 +0530

    fix build failure

--- src/test/HelloCI.Tests/UnitTest1.cs
+++ src/test/HelloCI.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace HelloCI.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("1", 2);
+            Assert.AreEqual("1", "1");
         }
     }
 }
